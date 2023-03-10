5aa80d4fb35c2f2ba6d1aae831d22ffdd43b8cae
xip0enix <108542693+xip0enix@users.noreply.github.com>
2023-03-10 18:33:59 +0100

    fix code

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,8 @@
 // Set the timer durations in minutes
-const workDuration = 25;
-const breakDuration = 5;
+const workDuration = 1;
+const breakDuration = 1;
 const longBreakDuration = 15;
-var pomodoroStreak = 0;
-
+let pomodoroStreak = 0;
 
 // Get the HTML elements
 const bodyElement = document.body;
@@ -13,105 +12,94 @@ const separatorElement = document.getElementById("separator");
 const startButton = document.getElementById("start");
 const resetButton = document.getElementById("reset");
 
-
 // Initialize the timer variables
 let currentDuration = workDuration;
 let secondsRemaining = currentDuration * 60;
 let intervalId;
 
-
 // Format the remaining time as MM:SS and update the HTML elements
 function updateTimer() {
-  const minutes = Math.floor(secondsRemaining / 60).toString().padStart(2, "0");
-  const seconds = (secondsRemaining % 60).toString().padStart(2, "0");
-  minutesElement.textContent = minutes;
-  secondsElement.textContent = seconds;
+const minutes = Math.floor(secondsRemaining / 60).toString().padStart(2, "0");
+const seconds = (secondsRemaining % 60).toString().padStart(2, "0");
+minutesElement.textContent = minutes;
+secondsElement.textContent = seconds;
 
-    // Update the page title with the remaining time
-  document.title = `${minutes}:${seconds} - Pomodoro Timer`;
+// Update the page title with the remaining time
+document.title = `${minutes}:${seconds} - Pomodoro Timer`;
 }
 
 // Start the timer
 function startTimer() {
+// Disable the start button
+startButton.disabled = true;
 
-  // Disable the start button
-  startButton.disabled = true;
-  
-    
-  // Update the timer immediately
-  updateTimer();
-  
+// Update the timer immediately
+updateTimer();
 
-  // Set up the interval to update the timer every second
-  intervalId = setInterval(() => {
-    // Decrement the remaining time
-    secondsRemaining--;
+// Set up the interval to update the timer every second
+intervalId = setInterval(() => {
+// Decrement the remaining time
+secondsRemaining--;
+// Update the timer display
+updateTimer();
 
-    // Update the timer display
-    updateTimer();
-
-    // Check if the timer has reached zero
-    if (secondsRemaining <= 0) {
-      // Stop the interval
-      playSound();
-      clearInterval(intervalId);
-
-      
-    
-      // Toggle the current duration and reset the seconds remaining
-      if (currentDuration === workDuration) {
-        pomodoroStreak++;
-        console.log(pomodoroStreak);
-       
-        //Pomodoro Long Break
-        if (pomodoroStreak === 4)
-        {
-          pomodoroStreak = 0;
-          currentDuration = longBreakDuration;
-          bodyElement.style.backgroundColor = "#3a3a3a";
-        }
-        else {
-          currentDuration = breakDuration;
-          bodyElement.style.backgroundColor = "#C9CBA3";
-        }
-        
-      } else {
-        currentDuration = workDuration;
-        bodyElement.style.backgroundColor = "#E26D5C";
-      }
-      secondsRemaining = currentDuration * 60;
-
-      // Update the timer display and enable the start button
-      updateTimer();
-      startButton.disabled = false;
-    }
-  }, 1000);
-}
-
-//Play Sound
-function playSound() {
-  let audio = new Audio("/audio/sound.wav");
-  audio.play();
-}
-function playCancel() {
-  let audio = new Audio("/audio/cancel.wav");
-  audio.play();
-}
-// Set up the click event listener for the start button
-startButton.addEventListener("click", startTimer);
-
-
-// Set up the click event listener for the reset button
-resetButton.addEventListener("click", () => {
-  // Stop the timer interval if it is running
-  playCancel();
+// Check if the timer has reached zero
+if (secondsRemaining <= 0) {
+  // Stop the interval and play sound
+  playSound();
   clearInterval(intervalId);
 
-  // Reset the current duration and seconds remaining
-  currentDuration = workDuration;
+  // Toggle the current duration and reset the seconds remaining
+  if (currentDuration === workDuration) {
+    pomodoroStreak++;
+    // Pomodoro Long Break
+    if (pomodoroStreak === 4) {
+      pomodoroStreak = 0;
+      currentDuration = longBreakDuration;
+      bodyElement.style.backgroundColor = "#3a3a3a";
+    } else {
+      currentDuration = breakDuration;
+      bodyElement.style.backgroundColor = "#C9CBA3";
+    }
+  } else {
+    currentDuration = workDuration;
+    bodyElement.style.backgroundColor = "#E26D5C";
+  }
   secondsRemaining = currentDuration * 60;
 
   // Update the timer display and enable the start button
   updateTimer();
   startButton.disabled = false;
+}
+}, 1000);
+}
+
+// Play Sound
+function playSound() {
+let audio = new Audio("/audio/sound.wav");
+audio.play();
+}
+
+// Play Cancel Sound
+function playCancel() {
+let audio = new Audio("/audio/cancel.wav");
+audio.play();
+}
+
+// Set up the click event listener for the start button
+startButton.addEventListener("click", startTimer);
+
+// Set up the click event listener for the reset button
+resetButton.addEventListener("click", () => {
+// Stop the timer interval if it is running
+playCancel();
+clearInterval(intervalId);
+
+// Reset the current duration and seconds remaining
+currentDuration = workDuration;
+secondsRemaining = currentDuration * 60;
+
+// Update the timer display and enable the start button
+updateTimer();
+startButton.disabled = false;
 });
