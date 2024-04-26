6fcebceffa7d2f51def314a34b1febcc2b0a7179
xip0enix <108542693+xip0enix@users.noreply.github.com>
2024-04-26 19:05:14 +0200

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -20,64 +20,80 @@ let intervalId;
 
 // Format the remaining time as MM:SS and update the HTML elements
 function updateTimer() {
-  const minutes = Math.floor(secondsRemaining / 60).toString().padStart(2, "0");
-  const seconds = (secondsRemaining % 60).toString().padStart(2, "0");
-  minutesElement.textContent = minutes;
-  secondsElement.textContent = seconds;
+const minutes = Math.floor(secondsRemaining / 60).toString().padStart(2, "0");
+const seconds = (secondsRemaining % 60).toString().padStart(2, "0");
+minutesElement.textContent = minutes;
+secondsElement.textContent = seconds;
 
-  document.title = `${minutes}:${seconds} - ${currentDuration === workDuration ? 'Time to focus!' : 'Time for a break!'}`;
+if (currentDuration === workDuration) {
+  // Update the page title with the remaining time
+  document.title = `${minutes}:${seconds} - Time to focus!`;
+} else {
+  // Pause
+  document.title = `${minutes}:${seconds} - Time for a break!`;
+}
+
 }
 
 // Start the timer
 function startTimer() {
+// Disable the start button
+startButton.disabled = true;
 
-  // Update the timer immediately
-  updateTimer();
+// Update the timer immediately
+updateTimer();
 
-  // Set up the interval to update the timer every second
-  intervalId = setInterval(() => {
-    // Decrement the remaining time
-    secondsRemaining--;
-    // Update the timer display
-    updateTimer();
+// Set up the interval to update the timer every second
+intervalId = setInterval(() => {
+// Decrement the remaining time
+secondsRemaining--;
+// Update the timer display
+updateTimer();
 
-    // Check if the timer has reached zero
-    if (secondsRemaining <= 0) {
-      // Stop the interval and play sound
-      playSound("https://github.com/xip0enix/pomodoro-timer/blob/main/audio/sound.mp3?raw=true");
-      clearInterval(intervalId);
+// Check if the timer has reached zero
+if (secondsRemaining <= 0) {
+  // Stop the interval and play sound
+  playSound();
+  clearInterval(intervalId);
 
-      // Calculate next break duration
-      const nextBreakDuration = pomodoroStreak === 3 ? longBreakDuration : breakDuration;
-
-      // Toggle the current duration and reset the seconds remaining
-      currentDuration = nextBreakDuration;
-      pomodoroStreak = currentDuration === workDuration ? pomodoroStreak : pomodoroStreak + 1; // Update streak on break start
-      secondsRemaining = currentDuration * 60;
-
-      // Update the timer display, round counter, and button state
-      updateTimer();
-      roundcounter.textContent = `#${pomodoroStreak + 1} ${currentDuration === workDuration ? 'Time to focus!' : 'Take a break!☕'}`;
-      bodyElement.style.backgroundColor = currentDuration === workDuration ? "#E26D5C" : (currentDuration === longBreakDuration ? "#3a3a3a" : "#C9CBA3");
-      startButton.disabled = false;
+  // Toggle the current duration and reset the seconds remaining
+  if (currentDuration === workDuration) {
+    // Pomodoro Long Break
+    if (pomodoroStreak === 3) { // Pomodoro-Zähler bei 0 beginnen, 4 Arbeitsphasen vor einer langen Pause
+      pomodoroStreak = 0;
+      roundcounter.textContent = "🎉";
+      currentDuration = longBreakDuration;
+      bodyElement.style.backgroundColor = "#3a3a3a";
     } else {
-      // Early exit for long break background color
-      if (currentDuration === longBreakDuration && bodyElement.style.backgroundColor === "#3a3a3a") {
-        return;  // Exit the interval if long break and color set
-      }
+      currentDuration = breakDuration;
+      bodyElement.style.backgroundColor = "#C9CBA3";
+      roundcounter.textContent = "#0" + (pomodoroStreak + 1) + " Take a break!☕"
     }
-  }, 1000);
+  } else {
+    pomodoroStreak++;
+    roundcounter.textContent = "#0" + (pomodoroStreak + 1) + " Time to focus!";
+    currentDuration = workDuration;
+    bodyElement.style.backgroundColor = "#E26D5C";
+  }
+  secondsRemaining = currentDuration * 60;
+
+  // Update the timer display and enable the start button
+  updateTimer();
+  startButton.disabled = false;
+}
+}, 1000);
 }
 
 // Play Sound
-function playSound(filePath) {
-  let audio = new Audio(filePath);
-  audio.play();
+function playSound() {
+let audio = new Audio("https://github.com/xip0enix/pomodoro-timer/blob/main/audio/sound.mp3?raw=true");
+audio.play();
 }
 
 // Play Cancel Sound
 function playCancel() {
-  playSound("https://github.com/xip0enix/pomodoro-timer/blob/main/audio/cancel.mp3?raw=true");
+let audio = new Audio("https://github.com/xip0enix/pomodoro-timer/blob/main/audio/cancel.mp3?raw=true");
+audio.play();
 }
 
 // Set up the click event listener for the start button
@@ -85,18 +101,17 @@ startButton.addEventListener("click", startTimer);
 
 // Set up the click event listener for the reset button
 resetButton.addEventListener("click", () => {
-  // Stop the timer interval if it is running
-  playCancel();
-  clearInterval(intervalId);
+// Stop the timer interval if it is running
+playCancel();
+clearInterval(intervalId);
 
-  // Reset the current duration and seconds remaining
-  currentDuration = workDuration;
-  secondsRemaining = currentDuration * 60;
-  roundcounter.textContent = `#${pomodoroStreak + 1} Time to focus!`;
-  bodyElement.style.backgroundColor = "#E26D5C";
-  pomodoroStreak = 0;
+// Reset the current duration and seconds remaining
+currentDuration = workDuration;
+secondsRemaining = currentDuration * 60;
+roundcounter.textContent = "#0" + (pomodoroStreak + 1) + " Time to focus!";
+bodyElement.style.backgroundColor = "#E26D5C";
 
-  // Update the timer display and enable the start button
-  updateTimer();
-  startButton.disabled = false;
+// Update the timer display and enable the start button
+updateTimer();
+startButton.disabled = false;
 });
